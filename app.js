288b38e5daf7ebe50b63931f6a645adb7ac07ef1
Weidson <wikaronascimento@gmail.com>
2019-10-13 15:31:11 -0300

    Correcao do tipo dom corpo da requisicao

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('view engine', 'pug');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 // app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.urlencoded({
+app.use(bodyParser.json({
   extended: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
